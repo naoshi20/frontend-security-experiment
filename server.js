f47f577d75d0fa9d2a081107a4b48cbe262890cd
naoshi20 <naoshi.st.paul@gmail.com>
2023-05-04 22:58:42 +0900

    CSP有効化

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const port = 3000;
 
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
+app.get("/csp", (req, res) => {
+    res.header("Content-Security-Policy", "script-src 'self'"); // CSPが有効になるためインラインスクリプトが実行されなくなる
+    res.render("csp");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on https://localhost:${port}`);
 });
