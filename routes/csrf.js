dec8276959db8fc903828a8462e4dbce9f7a0235
naoshi20 <naoshi.st.paul@gmail.com>
2023-05-06 20:51:35 +0900

    CSRFトークン対策としてDouble Submit Cookieを実装

--- routes/csrf.js
+++ routes/csrf.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const router = express.Router();
 
 // セッションにCookieを設定する
+// Note Session data is not saved in the cookie itself, just the session ID. Session data is stored server-side.
+// https://github.com/expressjs/session
 router.use(
     session({
         secret: "session",
@@ -28,15 +30,24 @@ router.post("/login", (req, res) => {
     console.log(req);
 
     const { username, password } = req.body;
-    // 検証用のため、ユーザー名とパスワードは固定
+    // 認証
     if (username !== "user1" || password !== "Passw0rd!#") {
         res.status(403);
         res.send("ログイン失敗");
         return;
     }
-    // セッションにユーザー名を格納
+    // 認証に成功したらセッションにユーザー名を格納
     sessionData = req.session;
     sessionData.username = username;
+
+    // CSRF対策のためにランダムなIDをcookieに保存させる
+    const crypto = require("crypto");
+    const token = crypto.randomUUID();
+    res.cookie("csrf_token"),
+        token,
+        {
+            secure: true,
+        };
 
     // CSRF検証用ページへリダイレクト
     res.redirect("/csrf_test.html");
@@ -49,6 +60,15 @@ router.post("/remit", (req, res) => {
     ) {
         res.status(403);
         res.send("ログインしていません。");
+        return;
+    }
+
+    // サーバ側のセッションには保存してないからcookieのトークンが正しいとするね。
+    // 偽物ならトークンは持ってても中身にはアクセスできないはずだからbodyに含めてリクエスト送れないっしょ。
+    // Cookieの性質：送り先ごとに情報を保存しており送り先にはCookieを必ず付与する。すなわち、罠サイトからのリクエストにもCookieは付与される。しかし、罠サイトではCookieの中身にアクセスすることはできない。
+    if (req.cookies["csrf_token"] !== req.body["csrf_token"]) {
+        res.status(400);
+        res.send("不正なリクエストです。");
         return;
     }
 
